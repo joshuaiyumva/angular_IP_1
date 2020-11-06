691fc031bbc3962ac138c5df16f4404e96589119
joshuaiyumva <joshuaiyumva@gmail.com>
2020-11-07 00:36:10 +0200

    add component to the constructor elements created

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,3 +1,4 @@
+import { Quote } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { Quotes } from '../quotes';
 
@@ -9,10 +10,12 @@ import { Quotes } from '../quotes';
 export class QuoteComponent implements OnInit {
 
   quotes: Quotes[] = [
-    new Quotes('Josue', '', 'Josue', '7/November/2020'),
-    new Quotes('Emile', '', 'Josue', '7/November/2020'),
-    new Quotes('Eliane', '', 'Josue', '7/November/2020'),
-    new Quotes('Emma', '', 'Josue', '7/November/2020'),
+    new Quotes('Nelson Mandela', 'The greatest glory in living lies not in never falling, but in rising every time we fall.', 'Josue', '7/November/2020'),
+    new Quotes('Walt Disney', 'The way to get started is to quit talking and begin doing.', 'Eliane', '7/November/2020'),
+    new Quotes('Steve Jobs', 'Your time is limited, so do not waste it living someone else life. Do not be trapped by dogma – which is living with the results of other peoples thinking.', 'Emile', '7/November/2020'),
+    new Quotes('Oprah Winfrey', 'If you look at what you have in life, you will always have more.If you look at what you do not have in life, you will never have enough.', 'Emma', '7/November/2020'),
+    new Quotes('James Cameron', 'If you set your goals ridiculously high and it is a failure, you will fail above everyone else success.','Josue','5/november/2020'),
+    new Quotes('John Lennon', 'Life is what happens when you are busy making other plans','Josue','7/November/2020')
 
   ];
   constructor() { }
